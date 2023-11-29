badc309c39a682bc97128cc229130f95d2a96c25
Aaro Koinsaari <aaro.koinsaari@proton.me>
2023-11-29 15:12:11 +0200

    Move table next to form

--- vt6.js
+++ vt6.js
@@ -24,7 +24,7 @@ const App = function (props) {
 
   /* jshint ignore:start */
   return (
-    <div>
+    <div className="flex-container">
       <LisaaJoukkue
         leimaustavat={state.kilpailu.leimaustavat}
         sarjat={state.kilpailu.sarjat}
